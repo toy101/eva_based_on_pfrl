4fb5ba72e8daf0b8cb2ca2bf9835f40893a6a98a
toy101 <toy101dev@gmail.com>
2021-01-24 16:56:38 +0900

    Delete unnecessary files.

--- trainer.sh
+++
@@ -1,9 +0,0 @@
-envs=(
-    # "PongNoFrameskip-v4"
-    "MsPacmanNoFrameskip-v4"
-    "QbertNoFrameskip-v4"
-)
-
-for env_name in "${envs[@]}"; do
-     python3 train.py --gpu 0 --agent EVA --env ${env_name}
-done
